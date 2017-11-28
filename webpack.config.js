62c2aa7b0790b116200feb312be285c92fa77ecd
malte <malte.frerichs@gmx.net>
2017-11-28 17:08:25 +0000

    Added Webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+let Encore = require('@symfony/webpack-encore');
+let webpack = require('webpack');
+let path = require('path');
+
+Encore
+	.setOutputPath("./dist/")
+	.setPublicPath("/dist")
+	.cleanupOutputBeforeBuild()
+
+	.addEntry('app', './src/glue.ts')
+	.addStyleEntry('style', './less/glue.less')
+
+	.autoProvidejQuery()
+
+	.enableTypeScriptLoader()
+	.enableLessLoader()
+
+	.createSharedEntry('vendor', [
+		'bootstrap/dist/js/bootstrap.bundle.js',
+		// 'moment',
+		// 'lodash',
+		// 'bootstrap-less/bootstrap/bootstrap.less',
+		'jquery',
+		// 'zone.js',
+		// 'reflect-metadata',
+	])
+
+	.addLoader({ test: /\.html/, loader: 'html-loader' })
+	// .addLoader({ test:  /component\.less/, loader: 'raw-loader!less-loader' })
+	// .addLoader({ test: /\.twig/, loader: 'twig-loader' })
+
+	// .addPlugin(new webpack.ContextReplacementPlugin(
+	// 	/angular(\\|\/)core(\\|\/)@angular/,
+	// 	root('./src') // location of your src
+	// ))
+
+	.enableSourceMaps(!Encore.isProduction())
+	.enableVersioning(Encore.isProduction())
+;
+
+function root(__path) {
+	return path.join(__dirname, __path);
+}
+
+module.exports = Encore.getWebpackConfig();
