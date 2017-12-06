30149aae80083003782b55d51fa94ff6670dc4ca
malte <malte.frerichs@gmx.net>
2017-12-06 14:06:29 +0000

    Added search container with animations

--- src/core/header.ts
+++ src/core/header.ts
@@ -1,9 +1,21 @@
 import 'jquery';
-import * as _ from 'lodash';
 
 export class cvHeader {
+
 	public static init(): void {
-		console.log("Init Header");
+		$('.cv-header-search').on('click', (event: JQuery.Event) => this.toggleSearch(event));
+		$('.cv-search-container').on('click', (event: JQuery.Event) => this.toggleSearch(event));
+	}
+
+	private static toggleSearch(event: JQuery.Event): void {
+		let searchContainer = $('.cv-search-container');
+
+		if (searchContainer.hasClass('cv-hide')) {
+			searchContainer.removeClass('cv-hide');
+			$('.cv-search-input').focus();
+		} else if($(event.target).hasClass('cv-search-container')) {
+			searchContainer.addClass('cv-hide');
+		}
 	}
 
 }
